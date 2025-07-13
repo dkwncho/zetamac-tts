7d81770672ce04c5384abdd57b85e2fe3c48b28a
dkwncho <dkwncho@gmail.com>
2025-07-13 09:19:44 +0900

    Main content script

---
+++ src/content/content.js
@@ -0,0 +1,73 @@
+class ZetamacTTS {
+  constructor() {
+    this.ttsManager = new TTSManager();
+    this.storageManager = new StorageManager();
+    this.uiManager = new UIManager();
+    this.equationReader = new EquationReader(this.ttsManager, this.uiManager);
+    this.updateInterval = null;
+  }
+
+  async initialize() {
+    await this.ttsManager.initialize();
+    await this.storageManager.initialize();
+
+    this.setupEventListeners();
+    this.uiManager.createButtons();
+    this.uiManager.updateTTSButton(this.storageManager.getTTSEnabled());
+
+    if (this.storageManager.getCoverEnabled()) {
+      this.uiManager.showEquationCover();
+    } else {
+      this.uiManager.hideEquationCover();
+    }
+
+    this.startEquationReading();
+  }
+
+  setupEventListeners() {
+    this.storageManager.onSettingsChange((settings) => {
+      if (settings.ttsEnabled !== undefined) {
+        this.uiManager.updateTTSButton(settings.ttsEnabled);
+        if (!settings.ttsEnabled) {
+          this.ttsManager.stop();
+        }
+      }
+
+      if (settings.coverEnabled !== undefined) {
+        if (settings.coverEnabled) {
+          this.uiManager.showEquationCover();
+        } else {
+          this.uiManager.hideEquationCover();
+        }
+      }
+    });
+
+    this.uiManager.onTTSToggle(() => {
+      const newState = !this.storageManager.getTTSEnabled();
+      this.storageManager.setTTSEnabled(newState);
+    });
+
+    this.uiManager.onCoverToggle(() => {
+      const newState = !this.storageManager.getCoverEnabled();
+      this.storageManager.setCoverEnabled(newState);
+    });
+  }
+
+  startEquationReading() {
+    this.updateInterval = setInterval(() => {
+      if (this.storageManager.getTTSEnabled()) {
+        this.equationReader.checkAndRead();
+      }
+    }, 100);
+  }
+
+  destroy() {
+    if (this.updateInterval) {
+      clearInterval(this.updateInterval);
+    }
+    this.ttsManager.stop();
+  }
+}
+
+const zetamacTTS = new ZetamacTTS();
+zetamacTTS.initialize();
